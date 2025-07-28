9338e5c9bae140031c0dbbe564549ac5482db8f9
tayloreynolds13 <tayloreynolds13@users.noreply.github.com>
2025-07-28 15:28:37 -0700

    cleeeeeeen up

--- run_gif_bpm_tool.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pip install pygame pygame_gui pillow
-python gif_bpm_sync_tool_v2.py
-pause
--- run_gif_bpm_tool.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip3 install pygame pygame_gui pillow
-python3 gif_bpm_sync_tool_v2.py
